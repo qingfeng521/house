a290295455f55a73ca23003b3a2d0046d873f7f8
孙永辉 <648047754@qq.com>
2019-03-27 17:02:13 +0800

    日志记录-----未完待续

--- api-gateway/src/main/java/com/imooc/house/api/dao/UserDao.java
+++ api-gateway/src/main/java/com/imooc/house/api/dao/UserDao.java
@@ -5,6 +5,7 @@ import com.imooc.house.api.config.GenericRest;
 import com.imooc.house.api.entity.UserEntity;
 import com.imooc.house.api.util.RestUtils;
 import com.netflix.discovery.converters.Auto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Repository;
@@ -17,6 +18,8 @@ public class UserDao {
     @Autowired
     private RestUtils restUtils;
 
+
+    @HystrixCommand(fallbackMethod = "getNullUser")
     public String getUserNameById(Long id){
         //RestUtils restUtils = new RestUtils();
         String orgUrl = "http://user/getUserName?id=" + id;
@@ -27,6 +30,16 @@ public class UserDao {
         return genericRest.resultGet("user",url, new ParameterizedTypeReference<RestResponse<String>>() {});
     }
 
+    /**
+     * 降级方法
+     * @param id
+     * @return
+     */
+    public String getNullUser(Long id){
+        return "";
+    }
+
+    @HystrixCommand(fallbackMethod = "getNullUser",ignoreExceptions = Exception.class)
     public UserEntity getUserByToken(String token){
         String url = "/getUserByToken?token="+token;
         return genericRest.resultGet("user",url, new ParameterizedTypeReference<RestResponse<UserEntity>>() {});
